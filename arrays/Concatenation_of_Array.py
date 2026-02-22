8b3a9411e387c0cbd3bd27225db2be9888cab69a
Revanthraghava <rvsnrevanth963@gmail.com>
2026-02-22 13:52:03 +0530

    Add arrays folder with Concatenation of Array solution

---
+++ arrays/Concatenation_of_Array.py
@@ -0,0 +1,13 @@
+# LeetCode 1929 - Concatenation of Array
+# https://leetcode.com/problems/concatenation-of-array/
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        ans = [0]*(2*n)
+        for i in range(n):
+            ans[i] = nums[i]
+            ans[i+n] = nums[i]
+        return ans
+    
+# Create result array of size 2n
+# Copy nums into first half and second half using index offset
