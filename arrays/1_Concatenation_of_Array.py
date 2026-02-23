a82158a53187ca0a23da40bd709e4e2306c102c6
Revanthraghava <rvsnrevanth963@gmail.com>
2026-02-23 23:45:00 +0530

    Solved: Contains Duplicate + update README

--- arrays/1_Concatenation_of_Array.py
+++ arrays/1_Concatenation_of_Array.py
@@ -1,5 +1,6 @@
 # LeetCode 1929 - Concatenation of Array
 # https://leetcode.com/problems/concatenation-of-array/
+from typing import List
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
         n = len(nums)
